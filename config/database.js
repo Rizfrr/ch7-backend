fb49ba79a7e49235b719659e2bb0da404ed7ed2c
Rizfrr <91199398+Rizfrr@users.noreply.github.com>
2022-06-03 15:41:10 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,30 +1,35 @@
 require("dotenv").config();
 
-const {DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
+const { DB_USERNAME, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 module.exports = {
-    development: {
-        username: DB_USERNAME,
-        password: DB_PASSWORD,
-        database: `${DB_NAME}`,
-        host: DB_HOST,
-        dialect: "postgres",
-        ssl: true,
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}`,
+    host: DB_HOST,
+    dialect: "postgres",
+    ssl: true,
+  },
+  test: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}_test`,
+    host: DB_HOST,
+    dialect: "postgres",
+    ssl: true,
+  },
+  production: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: `${DB_NAME}_production`,
+    host: DB_HOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-    test: {
-        username: DB_USERNAME,
-        password: DB_PASSWORD,
-        database: `${DB_NAME}_test`,
-        host: DB_HOST,
-        dialect: "postgres",
-        ssl: true,
-    },
-    production: {
-        username: DB_USERNAME,
-        password: DB_PASSWORD,
-        database: `${DB_NAME}_production`,
-        host: DB_HOST,
-        dialect: "postgres",
-        ssl: true,
-    },
+  },
 };
